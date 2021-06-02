042323c3bd0f51ebec04ba299139a2d12dc0f10a
Jan Polak <jan.polak@onmyway.cz>
2021-06-02 10:37:06 +0200

    day 28 MySQL testing

---
+++ day28-mysql/select.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM `countries`;
+
+
+SELECT *
+FROM `countries`
+WHERE `population` > 20000000;
+
+
+SELECT *
+FROM `countries`
+WHERE `population` > 20000000
+ORDER BY `population` DESC;
+
+
+SELECT *
+FROM `countries`
+WHERE `population` > 20000000
+ORDER BY `population` ASC
+LIMIT 10;
---
+++ day28-mysql/using-pdo.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'Country.php';
+
+try {
+    // connect to the database
+    $connection = new PDO(
+        'mysql:dbname=world;host=localhost;charset=utf8',
+        'root',
+        ''
+    );
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+
+var_dump($connection);
+
+
+$sql = "
+    SELECT *
+    FROM `countries`
+    WHERE `population` > 20000000
+    ORDER BY `population` ASC
+    LIMIT 5
+";
+
+// prepare the query, get the statement
+$statement = $connection->prepare($sql);
+
+var_dump($statement);
+
+// execute the statement
+$statement->execute();
+
+// specify how I want the results
+$statement->setFetchMode(PDO::FETCH_CLASS, 'Country');
+
+// get next row from the result
+$next_result = $statement->fetch();
+
+var_dump($next_result);
+
+$next_result = $statement->fetch();
+
+var_dump($next_result);
+
+$next_result = $statement->fetch();
+
+var_dump($next_result);
+
+
+$all_results = $statement->fetchAll();
+
+var_dump($all_results);
