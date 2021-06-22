91c89cf9646cdd2890936c292d3de236711a12e0
Slavo Kozar <slavo.kozar@gmail.com>
2021-06-22 13:32:43 +0200

    added useEffect demo to day 42 react demo app

--- day42-react/demo-app/src/HooksExample.js
+++ day42-react/demo-app/src/HooksExample.js
@@ -2,18 +2,48 @@ import {useEffect, useState} from "react";
 
 function HooksExample() {
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    console.log('rendering HooksExample component');
 
     useEffect(() => {
-        document.title = `You clicked ${count} times`;
+
+        console.log('I am loading data from server');
+
+    }, []);
+
+
+    useEffect(() => {
+        document.title = `You clicked ${count} and ${count2} times `;
         // document.title = 'You clicked ' + count + ' times';
-    });
+    }, [count, count2]);
+
+    useEffect(() => {
+        console.log(`You clicked ${count} times`);
+    }, [count]);
 
 
     return (
         <div>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
+            <button onClick={() => {
+                setCount(count + 1)
+
+                // the value count has not been changed yet
+                // document.title = `You clicked ${count} times`;
+            }}>
                 Click me
+            </button>
+
+
+            <p>You clicked {count2} times</p>
+            <button onClick={() => {
+                setCount2(count2 + 1)
+
+                // the value count has not been changed yet
+                // document.title = `You clicked ${count} times`;
+            }}>
+                Click me 2
             </button>
         </div>
     );
